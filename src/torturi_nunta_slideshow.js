1dd7c833da06b86cf97384332cce7e9d7c2d2533
Adela Veres <adela.veres@yahoo.ro>
2024-08-19 16:12:26 +0300

    Candybar Slideshow and added Marturii page

--- src/torturi_nunta_slideshow.js
+++ src/torturi_nunta_slideshow.js
@@ -1,6 +1,6 @@
 let slideIndexTorturi = 0;
 let slidesTorturi = document.getElementsByClassName("slide-item-torturi");
-let dotsTorturi = document.getElementsByClassName("wed-cake-dot");
+
 showSlidesCakes();
 
 // Next/previous controls
@@ -24,12 +24,6 @@ nextBtn.addEventListener('click', ()=>{
   plusSlidesCakes(1);
 });
 
-//Add dots listeners
-for (let i = 0; i < dotsTorturi.length; i++){
-  dotsTorturi[i].addEventListener('click',()=>{
-    currentSlideCakes(i+1);
-  });
-}
 
 function showSlideCakes(n) {
   let i;
@@ -37,16 +31,11 @@ function showSlideCakes(n) {
   for (i = 0; i < slidesTorturi.length; i++) {
     slidesTorturi[i].style.display = "none";
   }
-  //Deactivate all dots
-  for (i = 0; i < dotsTorturi.length; i++) {
-    dotsTorturi[i].className = dotsTorturi[i].className.replace(" active", "");
-  }
-  
+
   if (n > slidesTorturi.length) {slideIndexTorturi = 1}
   if (n < 1) {slideIndexTorturi = slidesTorturi.length}
 
   slidesTorturi[slideIndexTorturi-1].style.display = "block";
-  dotsTorturi[slideIndexTorturi-1].className += " active";
 
 }
 
@@ -54,8 +43,7 @@ function showSlidesCakes() {
   slideIndexTorturi++;
   showSlideCakes(slideIndexTorturi);
   setTimeout(showSlidesCakes, 3800);
-  
-  
+   
 }
 
 

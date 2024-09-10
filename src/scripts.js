acbfd5859d4e63b7749220ac937675ecf012a26e
Adela Veres <adela.veres@yahoo.ro>
2024-09-10 19:19:09 +0300

    Fixed navigation hamburger menu

--- src/scripts.js
+++ src/scripts.js
@@ -58,20 +58,5 @@ function showSlides() {
   
 }
 
-// Hamburger Menu
 
-let hamburger = document.getElementById("hamburger");
-let overlay = document.getElementById("overlay-screen");
-let closeBtn = document.getElementById("closebtn");
-
-hamburger.addEventListener('click', openHambMenu); 
-closeBtn.addEventListener('click', closeHambMenu);
-
-function openHambMenu(){
-  overlay.style.display = 'block';
-}
-
-function closeHambMenu(){
-  overlay.style.display = 'none';
-}
 

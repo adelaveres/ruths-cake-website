810921c5fbbc1c051f675db5fcdb46413c5d11c2
Adela Veres <adela.veres@yahoo.ro>
2024-10-28 18:47:32 +0200

    Finished functions for cake photo galleries

--- src/img_gallery.js
+++ src/img_gallery.js
@@ -2,37 +2,38 @@ import $ from 'jquery';
 
 
 $(document).ready(function() {
-    let anivPhotos = $(".torturi-aniversare").find(".img-wrapper");
+    let anivPhotos = Array.from($(".torturi-aniversare").find(".img-wrapper"));
+    let baptPhotos = Array.from($(".torturi-botez").find(".img-wrapper"));
+    let majPhotos = Array.from($(".torturi-majorate").find(".img-wrapper"));
+    let casPhotos = Array.from($(".torturi-caserola").find(".img-wrapper"));
+    addEventListenerToElements(anivPhotos);
+    addEventListenerToElements(baptPhotos);
+    addEventListenerToElements(majPhotos);
+    addEventListenerToElements(casPhotos);
 
 
-    for (let i = 0; i < anivPhotos.length; i++){
+    function addEventListenerToElements (elements){
 
-        let currPhoto = anivPhotos[i];
-        
-        currPhoto.addEventListener('click', ()=>{
-        
-            let currPhotoSrc = $(currPhoto).find("img").attr("src");
-            let modalWindow = $(currPhoto).find(".modal")[0];
+        elements.forEach(currPhotoWrapper => {
+
+            let currPhoto = $(currPhotoWrapper).find("img")[0];
+            let currPhotoSrc = $(currPhoto).attr("src");
+            let modalWindow = $(currPhotoWrapper).find(".modal")[0];
             let modalPhoto = $(modalWindow).find("#imgModal")[0];
             modalPhoto.src = currPhotoSrc;
-            
-            // Open Modal
-            modalWindow.style.display = 'block';
-        
             let closeBtn = $(modalWindow).find(".closebtn")[0];
 
-            closeBtn.addEventListener('click', ()=>{
-                // Close Modal
-                currPhoto.style.display = 'none';
+            currPhotoWrapper.addEventListener('click', ()=>{
+                // Open Modal
+                modalWindow.style.display = 'block';
             });
-        
+
+            closeBtn.addEventListener('click', (event)=>{
+                event.stopPropagation();
+                // Close Modal
+                modalWindow.style.display = 'none';
+            });
         });
-
-        
-
-        
-
-
     }
 
 });
